d9252f176120e50b2b992d8527fd5a5215251499
Berke <berkee991@gmail.com>
2024-09-12 14:43:17 +0300

    version1.02

--- InternTracker/Models/DTO/InternDTO.cs
+++ InternTracker/Models/DTO/InternDTO.cs
@@ -12,22 +12,9 @@ namespace InternTracker.Models.DTO
         public DateTime BirthDate { get; set; } 
         public DateTime InternStartDate { get; set; } 
         public DateTime InternEndDate { get; set; } 
-
-        public int Age { get; set; }
         public SchoolTypeEnum SchoolTypeStr { get; set; } 
         public string SchoolName { get; set; } 
         public string AcademicMajor { get; set; } 
        
-        InternDTO(Intern intern)
-        {
-
-            this.Age = AgeCalculate(intern.BirthDate);
-        }
-    
-        public int AgeCalculate(DateTime birthdate)
-        {
-            int ages=DateTime.Today.Year-birthdate.Year;
-            return Age;
-        }
     }
 }
